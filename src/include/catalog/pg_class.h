92a998842a8a759919e68b4bd16a4cff431315a8
Tomas Vondra <tomas@pgaddict.com>
2015-06-23 18:03:28 +0200

    add RELKIND_COLUMN_STORE to pg_class.h
    Maybe we should reuse the 'relam' to point to the colstore methods,
    but in that case it'd probably be better to stuff that into pg_am
    instead of using a separate catalog for colstores. OTOH colstore
    methods seem quite different from indexes, so stuffing that into
    pg_am seems silly. So maybe a separate AM column?

--- src/include/catalog/pg_class.h
+++ src/include/catalog/pg_class.h
@@ -158,6 +158,7 @@ DESCR("");
 #define		  RELKIND_COMPOSITE_TYPE  'c'		/* composite type */
 #define		  RELKIND_FOREIGN_TABLE   'f'		/* foreign table */
 #define		  RELKIND_MATVIEW		  'm'		/* materialized view */
+#define		  RELKIND_COLUMN_STORE	  'C'		/* columnar store */
 
 #define		  RELPERSISTENCE_PERMANENT	'p'		/* regular table */
 #define		  RELPERSISTENCE_UNLOGGED	'u'		/* unlogged permanent table */

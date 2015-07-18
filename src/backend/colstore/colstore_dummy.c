99df366852c4a071532ef970742e5d66ed1eb812
Tomas Vondra <tomas@pgaddict.com>
2015-07-18 02:18:43 +0200

    fixup! add a dummy cstore handler (doing nothing at the moment)
    - replace the cstore_insert() with a call to ExecColumnStoreInsert
    - add empty implementation to the dummy cstore

--- src/backend/colstore/colstore_dummy.c
+++ src/backend/colstore/colstore_dummy.c
@@ -5,11 +5,27 @@
 
 PG_FUNCTION_INFO_V1(cstore_dummy_handler);
 
+static void cstore_dummy_insert(Relation rel,
+				Relation colstorerel, ColumnStoreInfo *info,
+				int natts, Datum *values, bool *nulls,
+				ItemPointer tupleid);
 
 Datum
 cstore_dummy_handler(PG_FUNCTION_ARGS)
 {
         ColumnStoreRoutine *routine = makeNode(ColumnStoreRoutine);
 
+		routine->ExecColumnStoreInsert = cstore_dummy_insert;
+
         PG_RETURN_POINTER(routine);
+}
+
+
+static void
+cstore_dummy_insert(Relation rel,
+				Relation colstorerel, ColumnStoreInfo *info,
+				int natts, Datum *values, bool *nulls,
+				ItemPointer tupleid)
+{
+	elog(WARNING, "colstore dummy insert");
 }

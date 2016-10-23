6515d5f4e55f6ebffb67c22a2aea88b7d5c4b46b
Tomas Vondra <tomas@pgaddict.com>
2016-10-23 20:45:14 +0200

    fix: update CREATE STATISTICS in mv_mcv tests

--- src/test/regress/sql/mv_mcv.sql
+++ src/test/regress/sql/mv_mcv.sql
@@ -6,31 +6,31 @@ CREATE TABLE mcv_list (
 );
 
 -- unknown column
-CREATE STATISTICS s4 ON mcv_list (unknown_column) WITH (mcv);
+CREATE STATISTICS s4 WITH (mcv) ON (unknown_column) FROM mcv_list;
 
 -- single column
-CREATE STATISTICS s4 ON mcv_list (a) WITH (mcv);
+CREATE STATISTICS s4 WITH (mcv) ON (a) FROM mcv_list;
 
 -- single column, duplicated
-CREATE STATISTICS s4 ON mcv_list (a, a) WITH (mcv);
+CREATE STATISTICS s4 WITH (mcv) ON (a, a) FROM mcv_list;
 
 -- two columns, one duplicated
-CREATE STATISTICS s4 ON mcv_list (a, a, b) WITH (mcv);
+CREATE STATISTICS s4 WITH (mcv) ON (a, a, b) FROM mcv_list;
 
 -- unknown option
-CREATE STATISTICS s4 ON mcv_list (a, b, c) WITH (unknown_option);
+CREATE STATISTICS s4 WITH (unknown_option) ON (a, b, c) FROM mcv_list;
 
 -- missing MCV statistics
-CREATE STATISTICS s4 ON mcv_list (a, b, c) WITH (dependencies, max_mcv_items=200);
+CREATE STATISTICS s4 WITH (dependencies, max_mcv_items=200) ON (a, b, c) FROM mcv_list;
 
 -- invalid mcv_max_items value / too low
-CREATE STATISTICS s4 ON mcv_list (a, b, c) WITH (mcv, max_mcv_items=10);
+CREATE STATISTICS s4 WITH (mcv, max_mcv_items=10) ON (a, b, c) FROM mcv_list;
 
 -- invalid mcv_max_items value / too high
-CREATE STATISTICS s4 ON mcv_list (a, b, c) WITH (mcv, max_mcv_items=10000);
+CREATE STATISTICS s4 WITH (mcv, max_mcv_items=10000) ON (a, b, c) FROM mcv_list;
 
 -- correct command
-CREATE STATISTICS s4 ON mcv_list (a, b, c) WITH (mcv);
+CREATE STATISTICS s4 WITH (mcv) ON (a, b, c) FROM mcv_list;
 
 -- random data
 INSERT INTO mcv_list
@@ -87,7 +87,7 @@ CREATE TABLE mcv_list (
     c TEXT
 );
 
-CREATE STATISTICS s5 ON mcv_list (a, b, c) WITH (mcv);
+CREATE STATISTICS s5 WITH (mcv) ON (a, b, c) FROM mcv_list;
 
 -- random data
 INSERT INTO mcv_list
@@ -160,7 +160,7 @@ CREATE TABLE mcv_list (
     d TEXT
 );
 
-CREATE STATISTICS s6 ON mcv_list (a, b, c, d) WITH (mcv);
+CREATE STATISTICS s6 WITH (mcv) ON (a, b, c, d) FROM mcv_list;
 
 INSERT INTO mcv_list
      SELECT

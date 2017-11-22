e5b9fc752bce0c44aa51292db5224f8597e0d3ab
Tomas Vondra <tomas@2ndquadrant.com>
2017-11-22 01:13:58 +0100

    Introduce logical_work_mem to limit ReorderBuffer memory usage
    Instead of deciding to serialize a transaction merely based on the
    number of changes in that xact (toplevel or subxact), this makes
    the decisions based on amount of memory consumed by the changes.
    
    The amount of memory is defined by a new logical_work_mem GUC, so
    for example we can do this
    
        SET logical_work_mem = '128kB'
    
    to trigger very aggressive streaming. The minimum value is 64kB
    (i.e. it's lower than minimum value for maintenance_work_mem,
    which is 1MB).
    
    When adding a change to a transaction, we account for the size in
    two places. Firstly, in the ReorderBuffer, which is then used to
    decide if we reached the total memory limit. And secondly in the
    transaction the change belongs to, so that we can pick the largest
    transaction to evict (and serialize to disk).
    
    We still use max_changes_in_memory when loading changes serialized
    to disk. The trouble is we can't use the memory limit directly as
    there might be multiple subxact serialized, we need to read all of
    them but we don't know how many are there (and which subxact to
    read first).
    
    We do not serialize the ReorderBufferTXN entries, so if there is a
    transaction with many subxacts, most memory may be in this type of
    objects. Those records are not included in the memory accounting.
    
    We also do not account for INTERNAL_TUPLECID changes, which are
    kept in a separate list and not evicted from memory. Transactions
    with many CTID changes may consume significant amounts of memory,
    but we can't really do much about that.
    
    The current eviction algorithm is very simple - the transaction is
    picked merely by size, while it might be useful to also consider age
    (LSN) of the changes for example. With the new Generational memory
    allocator, evicting the oldest changes would make it more likely
    the memory gets actually pfreed.
    
    The logical_work_mem may be changed in two ways. In postgres.conf,
    which serves as the default for all publishers on that instance,
    and when creating the subscription, using a work_mem paramemter
    in the WITH clause (specifies number of kilobytes).

--- src/include/replication/reorderbuffer.h
+++ src/include/replication/reorderbuffer.h
@@ -17,6 +17,8 @@
 #include "utils/snapshot.h"
 #include "utils/timestamp.h"
 
+extern int	logical_work_mem;
+
 /* an individual tuple, stored in one chunk of memory */
 typedef struct ReorderBufferTupleBuf
 {
@@ -62,6 +64,9 @@ enum ReorderBufferChangeType
 	REORDER_BUFFER_CHANGE_INTERNAL_SPEC_CONFIRM
 };
 
+/* forward declaration */
+struct ReorderBufferTXN;
+
 /*
  * a single 'change', can be an insert (with one tuple), an update (old, new),
  * or a delete (old).
@@ -75,6 +80,9 @@ typedef struct ReorderBufferChange
 
 	/* The type of change. */
 	enum ReorderBufferChangeType action;
+
+	/* Transaction this change belongs to. */
+	struct ReorderBufferTXN *txn;
 
 	RepOriginId origin_id;
 
@@ -269,6 +277,11 @@ typedef struct ReorderBufferTXN
 	 */
 	dlist_node	node;
 
+	/*
+	 * Size of this transaction (changes currently in memory, in bytes).
+	 */
+	Size		size;
+
 } ReorderBufferTXN;
 
 /* so we can define the callbacks used inside struct ReorderBuffer itself */
@@ -351,6 +364,9 @@ struct ReorderBuffer
 	/* buffer for disk<->memory conversions */
 	char	   *outbuf;
 	Size		outbufsize;
+
+	/* memory accounting */
+	Size		size;
 };
 
 

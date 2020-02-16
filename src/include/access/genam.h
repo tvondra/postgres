36bb73722104dde632cab20094659dbf5fd24f94
Peter Geoghegan <pg@bowt.ie>
2020-02-16 01:16:02 -0800

    DEBUG: Show index values in pageinspect
    This is not intended for commit.  It is included as a convenience for
    reviewers.

--- src/include/access/genam.h
+++ src/include/access/genam.h
@@ -249,6 +249,8 @@ extern void index_get_prefetch_stats(IndexScanDesc scan, int64 *accum,
 extern IndexScanDesc RelationGetIndexScan(Relation indexRelation,
 										  int nkeys, int norderbys);
 extern void IndexScanEnd(IndexScanDesc scan);
+extern char *BuildIndexValueDescriptionNatts(Relation indexRelation, int indnkeyatts,
+											 const Datum *values, const bool *isnull);
 extern char *BuildIndexValueDescription(Relation indexRelation,
 										const Datum *values, const bool *isnull);
 extern TransactionId index_compute_xid_horizon_for_tuples(Relation irel,

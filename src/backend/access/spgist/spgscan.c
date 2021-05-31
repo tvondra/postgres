hanzoai/sql

Fix more confusion in SP-GiST.
spg_box_quad_leaf_consistent unconditionally returned the leaf
datum as leafValue, even though in its usage for poly_ops that
value is of completely the wrong type.

In versions before 12, that was harmless because the core code did
nothing with leafValue in non-index-only scans ... but since commit
2a6368343, if we were doing a KNN-style scan, spgNewHeapItem would
unconditionally try to copy the value using the wrong datatype
parameters.  Said copying is a waste of time and space if we're not
going to return the data, but it accidentally failed to fail until
I fixed the datatype confusion in ac9099fc1.

Hence, change spgNewHeapItem to not copy the datum unless we're
actually going to return it later.  This saves cycles and dodges
the question of whether lossy opclasses are returning the right
type.  Also change spg_box_quad_leaf_consistent to not return
data that might be of the wrong type, as insurance against
somebody introducing a similar bug into the core code in future.

It seems like a good idea to back-patch these two changes into
v12 and v13, although I'm afraid to change spgNewHeapItem's
mistaken idea of which datatype to use in those branches.

Per buildfarm results from ac9099fc1.

Discussion: https://postgr.es/m/3728741.1617381471@sss.pgh.pa.us

src/backend/access/spgist/spgscan.c
@@ -446,10 +446,22 @@ spgNewHeapItem(SpGistScanOpaque so, int level, ItemPointer heapPtr,
 
 	item->level = level;
 	item->heapPtr = *heapPtr;
-	/* copy value to queue cxt out of tmp cxt */
-	item->value = isnull ? (Datum) 0 :
-		datumCopy(leafValue, so->state.attLeafType.attbyval,
-				  so->state.attLeafType.attlen);
+
+	/*
+	 * If we need the reconstructed value, copy it to queue cxt out of tmp
+	 * cxt.  Caution: the leaf_consistent method may not have supplied a value
+	 * if we didn't ask it to, and mildly-broken methods might supply one of
+	 * the wrong type.  Also, while the correct leafValue type is attType not
+	 * leafType, pre-v14 Postgres versions have historically used attLeafType
+	 * here; let's not confuse matters even more by changing that in a minor
+	 * release.
+	 */
+	if (so->want_itup)
+		item->value = isnull ? (Datum) 0 :
+			datumCopy(leafValue, so->state.attLeafType.attbyval,
+					  so->state.attLeafType.attlen);
+	else
+		item->value = (Datum) 0;
 	item->traversalValue = NULL;
 	item->isLeaf = true;
 	item->recheck = recheck;

tvondra/postgres

src/backend/access/spgist/spgscan.c
@@ -463,11 +463,19 @@ spgNewHeapItem(SpGistScanOpaque so, int level, ItemPointer heapPtr,
 
 	item->level = level;
 	item->heapPtr = *heapPtr;
-	/* copy value to queue cxt out of tmp cxt */
-	/* caution: "leafValue" is of type attType not leafType */
-	item->value = isnull ? (Datum) 0 :
-		datumCopy(leafValue, so->state.attType.attbyval,
-				  so->state.attType.attlen);
+
+	/*
+	 * If we need the reconstructed value, copy it to queue cxt out of tmp
+	 * cxt.  Caution: the leaf_consistent method may not have supplied a value
+	 * if we didn't ask it to, and mildly-broken methods might supply one of
+	 * the wrong type.  The correct leafValue type is attType not leafType.
+	 */
+	if (so->want_itup)
+		item->value = isnull ? (Datum) 0 :
+			datumCopy(leafValue, so->state.attType.attbyval,
+					  so->state.attType.attlen);
+	else
+		item->value = (Datum) 0;
 	item->traversalValue = NULL;
 	item->isLeaf = true;
 	item->recheck = recheck;
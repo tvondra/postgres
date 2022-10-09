b745fb8d50450d4ae48cddc57ebddcf714682bb4
Tomas Vondra <tomas.vondra@postgresql.org>
2022-10-09 11:33:37 +0200

    Allow BRIN indexes to produce sorted output
    Some BRIN indexes can be used to produce sorted output, by using the
    range information to sort tuples incrementally. This is particularly
    interesting for LIMIT queries, which only need to scan the first few
    rows, and alternative plans (e.g. Seq Scan + Sort) have a very high
    startup cost.
    
    Of course, if there are e.g. BTREE indexes this is going to be slower,
    but people are unlikely to have both index types on the same column.
    
    This is disabled by default, use enable_brinsort GUC to enable it.

--- src/include/nodes/plannodes.h
+++ src/include/nodes/plannodes.h
@@ -501,6 +501,38 @@ typedef struct IndexOnlyScan
 	ScanDirection indexorderdir;	/* forward or backward or don't care */
 } IndexOnlyScan;
 
+/*
+ * XXX Does it make sense (is it possible) to have a sort by more than one
+ * column, using a BRIN index?
+ */
+typedef struct BrinSort
+{
+	Scan		scan;
+	Oid			indexid;		/* OID of index to scan */
+	List	   *indexqual;		/* list of index quals (usually OpExprs) */
+	List	   *indexqualorig;	/* the same in original form */
+	ScanDirection indexorderdir;	/* forward or backward or don't care */
+
+	/* number of sort-key columns */
+	int			numCols;
+
+	/* attnums in the index */
+	AttrNumber *attnums pg_node_attr(array_size(numCols));
+
+	/* their indexes in the target list */
+	AttrNumber *sortColIdx pg_node_attr(array_size(numCols));
+
+	/* OIDs of operators to sort them by */
+	Oid		   *sortOperators pg_node_attr(array_size(numCols));
+
+	/* OIDs of collations */
+	Oid		   *collations pg_node_attr(array_size(numCols));
+
+	/* NULLS FIRST/LAST directions */
+	bool	   *nullsFirst pg_node_attr(array_size(numCols));
+
+} BrinSort;
+
 /* ----------------
  *		bitmap index scan node
  *

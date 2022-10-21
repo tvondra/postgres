d52ae732a67c52a8928c089cb7f7b181094aa8ea
Tomas Vondra <tomas.vondra@postgresql.org>
2022-10-22 01:39:39 +0200

    wip: adaptive watermark step
    Another option it to adjust the watermark step based on past tuplesort
    executions, and either increase or decrease the step, based on whether
    the sort was in-memory or on-disk, etc.
    
    To do this, set the GUC to -1:
    
      SET brinsort_watermark_step = -1;

--- src/backend/access/brin/brin_minmax.c
+++ src/backend/access/brin/brin_minmax.c
@@ -47,9 +47,6 @@ static FmgrInfo *minmax_get_strategy_procinfo(BrinDesc *bdesc, uint16 attno,
 											  Oid subtype, uint16 strategynum);
 
 
-/* print info about ranges */
-#define BRINSORT_DEBUG
-
 Datum
 brin_minmax_opcinfo(PG_FUNCTION_ARGS)
 {
@@ -1989,7 +1986,7 @@ brin_minmax_scan_add_tuple(BrinRangeScanDesc *scan, TupleTableSlot *slot,
 	scan->nranges++;
 }
 
-#ifdef BRINSORT_DEBUG
+#ifdef BRIN_SORT_DEBUG
 /*
  * brin_minmax_scan_next
  *		Return the next BRIN range information from the tuplestore.
@@ -2198,7 +2195,7 @@ brin_minmax_ranges(PG_FUNCTION_ARGS)
 	/* do the sort and any necessary post-processing */
 	brin_minmax_scan_finalize(brscan);
 
-#ifdef BRINSORT_DEBUG
+#ifdef BRIN_SORT_DEBUG
 	brin_minmax_scan_dump(brscan);
 #endif
 

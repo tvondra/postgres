cba0cd1654f94ce8785554466038e4f27b181685
Tomas Vondra <tomas.vondra@postgresql.org>
2023-03-20 20:06:58 +0100

    index-only scans and selectivity

--- src/include/optimizer/paths.h
+++ src/include/optimizer/paths.h
@@ -81,6 +81,7 @@ extern bool indexcol_is_bool_constant_for_query(PlannerInfo *root,
 extern bool match_index_to_operand(Node *operand, int indexcol,
 								   IndexOptInfo *index);
 extern void check_index_predicates(PlannerInfo *root, RelOptInfo *rel);
+extern bool check_index_only(RelOptInfo *rel, IndexOptInfo *index);
 
 /*
  * tidpath.h

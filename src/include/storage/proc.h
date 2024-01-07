c42df6b2a04a8228507081bf30b796ff382943ab
Tomas Vondra <tomas@2ndquadrant.com>
2024-01-07 22:48:22 +0100

    Increase the number of fastpath locks
    The 16 fastpath locks as defined by FP_LOCK_SLOTS_PER_BACKEND may be a
    bottleneck with many partitions (or relations in general - e.g. large
    joins). This applies especially to many-core systems, but not only.
    
    This increases the numeber of fastpath slots per backend from to 1024
    (from 16). This is implemented as hash table of 64 "entries", where an
    entry is essentially the current array of 16 fastpath slots. A relation
    is mapped to one of the 64 entries by hash(relid), and then following
    the existing lookup / eviction logic.
    
    This provides better locality than open-addressing hash tables.
    
    It's not clear if 1024 is the right trade-off. It does make the PGPROC
    entry larger, but it's already quite large and no regressions were
    observed during benchmarking.

--- src/include/storage/proc.h
+++ src/include/storage/proc.h
@@ -82,8 +82,9 @@ struct XidCache
  * rather than the main lock table.  This eases contention on the lock
  * manager LWLocks.  See storage/lmgr/README for additional details.
  */
-#define		FP_LOCK_SLOTS_PER_BACKEND 16
-
+#define		FP_LOCK_GROUPS_PER_BACKEND	64
+#define		FP_LOCK_SLOTS_PER_GROUP		16		/* don't change */
+#define		FP_LOCKS_PER_BACKEND		(FP_LOCK_SLOTS_PER_GROUP * FP_LOCK_GROUPS_PER_BACKEND)
 /*
  * An invalid pgprocno.  Must be larger than the maximum number of PGPROC
  * structures we could possibly have.  See comments for MAX_BACKENDS.
@@ -288,8 +289,8 @@ struct PGPROC
 
 	/* Lock manager data, recording fast-path locks taken by this backend. */
 	LWLock		fpInfoLock;		/* protects per-backend fast-path state */
-	uint64		fpLockBits;		/* lock modes held for each fast-path slot */
-	Oid			fpRelId[FP_LOCK_SLOTS_PER_BACKEND]; /* slots for rel oids */
+	uint64		fpLockBits[FP_LOCK_GROUPS_PER_BACKEND];		/* lock modes held for each fast-path slot */
+	Oid			fpRelId[FP_LOCKS_PER_BACKEND]; /* slots for rel oids */
 	bool		fpVXIDLock;		/* are we holding a fast-path VXID lock? */
 	LocalTransactionId fpLocalTransactionId;	/* lxid for fast-path VXID
 												 * lock */

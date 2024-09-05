6b22fa1733814cbacb5431db95842ba261b699d9
Tomas Vondra <tomas@2ndquadrant.com>
2024-09-05 10:37:37 +0200

    fixup

--- src/backend/storage/ipc/ipci.c
+++ src/backend/storage/ipc/ipci.c
@@ -156,8 +156,6 @@ CalculateShmemSize(int *num_semaphores)
 	/* include additional requested shmem from preload libraries */
 	size = add_size(size, total_addin_request);
 
-	elog(LOG, "CalculateShmemSize %lu", size);
-
 	/* might as well round it off to a multiple of a typical page size */
 	size = add_size(size, 8192 - (size % 8192));
 

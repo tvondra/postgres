9f650f9d4280f2958620695a6c7ad8e22c01a271
Tomas Vondra <tomas@vondra.me>
2024-09-30 22:48:12 +0200

    WIP: index batching / prefetching
    Allows the index AM to provide items (TIDs and tuples) in batches, which
    is then used to implement prefetching of heap tuples in index scans
    (including index-only scans). This is similar to prefetching already
    done in bitmap scans, and can result in significant speedups.
    
    The index AM may implement an optional "amgetbatch" callback, returning
    a batch of items. The indexam.c code then handles this transparently
    through the existing "getnext" interface.
    
    It is up to the index AM to return only batches that it can handle
    internally. For example, most of the later patches adding support for
    batching to relevant index AMs (btree, hash, gist, sp-gist) restrict the
    batches to a single leaf page. This makes implementation of batching
    much simpler, with only minimal changes to the index AMs, but it's not a
    hard requirement. The index AM can produce batches spanning arbitrary
    number of leaf pages. This is left as a possible future improvement.
    
    Most of the batching/prefetching logic happens in indexam.c. This means
    the executor code can continue to call the interface just like before.
    
    The only "violation" happens in index-only scans, which need to check
    the visibility map both when the prefetching pages (we don't want to
    prefetch pages that are unnecessary) and later when reading the data.
    For cached data the visibility map checks can be fairly expensive, so
    it's desirable to keep and reuse the result of the first check.
    
    At the moment, the prefetching does not handle mark/restore plans. This
    is doable, but requires additional synchronization between the batching
    and index AM code in the "opposite direction".
    
    This patch does not actually add batching to any of the index AMs, it's
    just the common infrastructure.
    
    TODO Add the new index AM callback to sgml docs.

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -1653,6 +1653,7 @@ typedef struct
  *		OrderByTypByVals   is the datatype of order by expression pass-by-value?
  *		OrderByTypLens	   typlens of the datatypes of order by expressions
  *		PscanLen		   size of parallel index scan descriptor
+ *		CanBatch		   batching (and prefetching) enabled
  * ----------------
  */
 typedef struct IndexScanState
@@ -1680,6 +1681,10 @@ typedef struct IndexScanState
 	bool	   *iss_OrderByTypByVals;
 	int16	   *iss_OrderByTypLens;
 	Size		iss_PscanLen;
+
+	/* batching/prefetching enabled? */
+	bool		iss_CanBatch;
+
 } IndexScanState;
 
 /* ----------------
@@ -1701,6 +1706,7 @@ typedef struct IndexScanState
  *		PscanLen		   size of parallel index-only scan descriptor
  *		NameCStringAttNums attnums of name typed columns to pad to NAMEDATALEN
  *		NameCStringCount   number of elements in the NameCStringAttNums array
+ *		CanBatch		   batching (and prefetching) enabled
  * ----------------
  */
 typedef struct IndexOnlyScanState
@@ -1722,6 +1728,7 @@ typedef struct IndexOnlyScanState
 	Size		ioss_PscanLen;
 	AttrNumber *ioss_NameCStringAttNums;
 	int			ioss_NameCStringCount;
+	bool		ioss_CanBatch;
 } IndexOnlyScanState;
 
 /* ----------------

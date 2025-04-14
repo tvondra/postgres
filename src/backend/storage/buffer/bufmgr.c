9f840445b94da1cbe8c8f677bb20a897619e7dca
Tomas Vondra <tomas@vondra.me>
2025-04-14 22:23:31 +0200

    buffmgr: super-pinning - BROKEN
    Sometimes pages may get re-pinned extremely frequently, depending on the
    query plan. For example with a NL + index scan, the btree root page (and
    the immediate child pages) get very hot. Even the read-only pgbench will
    hit this issue. And it's a concurrency issue, because all the backends
    will hit the same cacheline.
    
    This can be quantified by running the same benchmark with all clients on
    a single instance vs. multiple instances. And then also single database
    vs. multiple databases (in one instance). The "single instance" case
    will often perform much worse than the other cases, exactly because of
    the extra contention.
    
    By definition, this affects only a tiny number of hot pages - btree root
    pages, etc. For sequences this would matter only for writes (advances of
    sequences), so not very likely.
    
    The idea is to detect pages where it matters, and use a cheaper pinning
    protocol, roughly similar to the fast-path locking. Backends would have
    a small number of local pin slots, and we'd use this for the hot pages.
    
    The question is how we'd pick the hot pages - for fast-path locks we use
    the first-come strategy, but for pins that might not work, and we should
    use it for the hot pages. Or maybe not, and we should simply size the
    local buffer to keep all pins?
    
    The idea is sound, but the current code is wrong. Would be helpful even
    without NUMA.

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -2846,7 +2846,7 @@ ExtendBufferedRelShared(BufferManagerRelation bmr,
 		if (lock)
 			LWLockAcquire(BufferDescriptorGetContentLock(buf_hdr), LW_EXCLUSIVE);
 
-		TerminateBufferIO(buf_hdr, false, BM_VALID, true, false);
+		TerminateBufferIO(buf_hdr, false, BM_VALID | BM_SUPER, true, false);
 	}
 
 	pgBufferUsage.shared_blks_written += extend_by;
@@ -3066,6 +3066,13 @@ PinBuffer(BufferDesc *buf, BufferAccessStrategy strategy)
 		old_buf_state = pg_atomic_read_u32(&buf->state);
 		for (;;)
 		{
+			if (old_buf_state & BM_SUPER)
+			{
+				result = (old_buf_state & BM_VALID) != 0;
+				VALGRIND_MAKE_MEM_DEFINED(BufHdrGetBlock(buf), BLCKSZ);
+				break;
+			}
+
 			if (old_buf_state & BM_LOCKED)
 				old_buf_state = WaitBufHdrUnlocked(buf);
 
@@ -3181,7 +3188,8 @@ PinBuffer_Locked(BufferDesc *buf)
 	 */
 	buf_state = pg_atomic_read_u32(&buf->state);
 	Assert(buf_state & BM_LOCKED);
-	buf_state += BUF_REFCOUNT_ONE;
+	if (!(buf_state & BM_SUPER))
+		buf_state += BUF_REFCOUNT_ONE;
 	UnlockBufHdr(buf, buf_state);
 
 	b = BufferDescriptorGetBuffer(buf);
@@ -3284,6 +3292,12 @@ UnpinBufferNoOwner(BufferDesc *buf)
 		old_buf_state = pg_atomic_read_u32(&buf->state);
 		for (;;)
 		{
+			if (old_buf_state & BM_SUPER)
+			{
+				buf_state = old_buf_state;
+				break;
+			}
+
 			if (old_buf_state & BM_LOCKED)
 				old_buf_state = WaitBufHdrUnlocked(buf);
 
@@ -4483,8 +4497,11 @@ BufferGetLSNAtomic(Buffer buffer)
 
 	bufHdr = GetBufferDescriptor(buffer - 1);
 	buf_state = LockBufHdr(bufHdr);
+	if (0)
+		buf_state = LockBufHdr(bufHdr);
 	lsn = PageGetLSN(page);
-	UnlockBufHdr(bufHdr, buf_state);
+	if (0)
+		UnlockBufHdr(bufHdr, buf_state);
 
 	return lsn;
 }
@@ -5589,9 +5606,15 @@ LockBuffer(Buffer buffer, int mode)
 	buf = GetBufferDescriptor(buffer - 1);
 
 	if (mode == BUFFER_LOCK_UNLOCK)
-		LWLockRelease(BufferDescriptorGetContentLock(buf));
+	{
+		if (!(pg_atomic_read_u32(&buf->state) & BM_SUPER))
+			LWLockRelease(BufferDescriptorGetContentLock(buf));
+	}
 	else if (mode == BUFFER_LOCK_SHARE)
-		LWLockAcquire(BufferDescriptorGetContentLock(buf), LW_SHARED);
+	{
+		if (!(pg_atomic_read_u32(&buf->state) & BM_SUPER))
+			LWLockAcquire(BufferDescriptorGetContentLock(buf), LW_SHARED);
+	}
 	else if (mode == BUFFER_LOCK_EXCLUSIVE)
 		LWLockAcquire(BufferDescriptorGetContentLock(buf), LW_EXCLUSIVE);
 	else
@@ -6594,6 +6617,8 @@ EvictUnpinnedBufferInternal(BufferDesc *desc, bool *buffer_flushed)
 
 	/* This will return false if it becomes dirty or someone else pins it. */
 	result = InvalidateVictimBuffer(desc);
+
+	StrategyFreeBuffer(desc);
 
 	UnpinBuffer(desc);
 

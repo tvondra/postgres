935054c0b4781aedc8eb17ed2f83c290da0ed038
Tomas Vondra <tomas@vondra.me>
2025-04-15 17:04:37 +0200

    NUMA: shared_memory_populate
    Seems to be necessary for the interleaving to work reliably. But even if
    it was not required for interleaving to work, it's independently useful
    for benchmarking, as it makes warmup shorter.

--- src/backend/utils/misc/guc_tables.c
+++ src/backend/utils/misc/guc_tables.c
@@ -2138,6 +2138,16 @@ struct config_bool ConfigureNamesBool[] =
 	},
 
 	{
+		{"numa_shmem_populate", PGC_POSTMASTER, DEVELOPER_OPTIONS,
+			gettext_noop("Enables prefaulting all shared memory pages at mmap time."),
+			gettext_noop("When enables, the page tables get prefaulted for the segment."),
+		},
+		&numa_shmem_populate,
+		true,
+		NULL, NULL, NULL
+	},
+
+	{
 		{"sync_replication_slots", PGC_SIGHUP, REPLICATION_STANDBY,
 			gettext_noop("Enables a physical standby to synchronize logical failover replication slots from the primary server."),
 		},

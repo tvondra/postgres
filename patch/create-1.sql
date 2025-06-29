a3c28caba46578047ff516197769c3ce86ab2d67
Tomas Vondra <tomas@vondra.me>
2025-06-29 21:42:59 +0200

    fixup example

--- patch/create-1.sql
+++ patch/create-1.sql
@@ -8,11 +8,11 @@ create table dim7 (id int primary key, val7 text);
 
 create table t (id serial primary key,
                 id1 int references dim1(id),
-                id2 int references dim1(id),
-                id3 int references dim1(id),
-                id4 int references dim1(id),
-                id5 int references dim1(id),
-                id6 int references dim1(id),
-                id7 int references dim1(id));
+                id2 int references dim2(id),
+                id3 int references dim3(id),
+                id4 int references dim4(id),
+                id5 int references dim5(id),
+                id6 int references dim6(id),
+                id7 int references dim7(id));
 
 vacuum analyze;

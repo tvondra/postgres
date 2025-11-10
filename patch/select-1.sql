eb0898b37928028eac955cf4b0b12d855031cb5e
Tomas Vondra <tomas@vondra.me>
2025-11-11 00:49:02 +0100

    WIP

--- patch/select-1.sql
+++ patch/select-1.sql
@@ -7,3 +7,12 @@ select * from t
     join dim5 on (dim5.id = id5)
     join dim6 on (dim6.id = id6)
     join dim7 on (dim7.id = id7);
+
+select * from t
+    left join dim1 on (dim1.id = id1)
+    left join dim2 on (dim2.id = id2)
+    left join dim3 on (dim3.id = id3)
+    left join dim4 on (dim4.id = id4)
+    left join dim5 on (dim5.id = id5)
+    left join dim6 on (dim6.id = id6)
+    left join dim7 on (dim7.id = id7);

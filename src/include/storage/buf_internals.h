86dcdb4dded1e5bad5397eef25aba47034ce2591
Tomas Vondra <tomas@vondra.me>
2025-11-11 12:03:32 +0100

    clock-sweep: basic partitioning
    Partitions the "clock-sweep" algorithm to work on individual partitions,
    one by one. Each backend process is mapped to one "home" partition, with
    an independent clock hand. This reduces contention for workloads with
    significant buffer pressure.
    
    The patch extends the "pg_buffercache_partitions" view to include
    information about the clock-sweep activity.
    
    Note: This needs some sort of "balancing" when one of the partitions is
    much busier than the rest (e.g. because there's a single backend consuming
    a lot of buffers from it).
    
    Note: There's a problem with some tests running out of unpinned buffers,
    due to (intentionally) setting shared buffers very low. That happens
    because StrategyGetBuffer() only searches a single partition, and it
    has a couple more issues.

--- src/include/storage/buf_internals.h
+++ src/include/storage/buf_internals.h
@@ -569,7 +569,9 @@ extern BufferDesc *StrategyGetBuffer(BufferAccessStrategy strategy,
 extern bool StrategyRejectBuffer(BufferAccessStrategy strategy,
 								 BufferDesc *buf, bool from_ring);
 
-extern int	StrategySyncStart(uint32 *complete_passes, uint32 *num_buf_alloc);
+extern void StrategySyncPrepare(int *num_parts, uint32 *num_buf_alloc);
+extern int	StrategySyncStart(int partition, uint32 *complete_passes,
+							  int *first_buffer, int *num_buffers);
 extern void StrategyNotifyBgWriter(int bgwprocno);
 
 extern Size StrategyShmemSize(void);
@@ -615,5 +617,6 @@ extern int	BufferPartitionCount(void);
 extern int	BufferPartitionNodes(void);
 extern void BufferPartitionGet(int idx, int *num_buffers,
 							   int *first_buffer, int *last_buffer);
+extern void BufferPartitionParams(int *num_partitions);
 
 #endif							/* BUFMGR_INTERNALS_H */

c18653d312037cd70bb3a39d38517f743fafb61b
Peter Geoghegan <pg@bowt.ie>
2025-11-15 14:03:58 -0500

    Add heapam index scan I/O prefetching.
    This commit implements I/O prefetching for index scans (and index-only
    scans that require heap fetches). This was made possible by the recent
    addition of batching interfaces to both the table AM and index AM APIs.
    
    The amgetbatch index AM interface provides batches of matching TIDs
    (rather than one tuple at a time), each of which must be taken from
    index tuples that appear together on a single index page.  This allows
    multiple batches to be held open simultaneously.  Giving the table AM an
    explicit understanding of index AM concepts/index page boundaries allows
    it to consider all of the relevant costs and benefits.
    
    Prefetching is implemented using a prefetching position under the
    control of the table AM and core code.  This is closely related to the
    scan position added by commit FIXME, which introduced the amgetbatch
    interface.  A read stream callback advances the read stream as needed to
    provide sufficiently many heap block numbers to maintain the read
    stream's target prefetch distance.
    
    Testing has shown that index prefetching can make index scans much
    faster.  Large range scans that return many tuples can be as much as 35x
    faster.
    
    An important goal of the amgetbatch design is to enable the table AM's
    read stream callback to advance its prefetch position using TIDs that
    appear on a leaf page that's ahead of the current scan position's leaf
    page.  This is crucial with scans of indexes where each leaf page
    happens to have relatively few distinct heap blocks among its matching
    TIDs (as well as with scans with leaf pages that have relatively few
    total matching items).  Index scans can have as many as 64 open batches,
    which testing has shown to be about the maximum number that can ever be
    useful.  Batches are maintained in scan order using a simple ring buffer
    data structure.
    
    In rare cases where the scan exceeds this quasi-arbitrary limit of 64,
    the read stream is temporarily paused.  Prefetching (via the read
    stream) is resumed only after the scan position advances beyond its
    current open batch and then frees and removes the batch from the scan's
    batch ring buffer.  Testing has shown that it isn't very common for
    scans to hold open more than about 10 batches to get the desired I/O
    prefetch distance.
    
    Author: Tomas Vondra <tomas@vondra.me>
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Reviewed-By: Thomas Munro <thomas.munro@gmail.com>
    Discussion: https://postgr.es/m/cf85f46f-b02f-05b2-5248-5000b894ebab@enterprisedb.com

--- src/backend/access/index/indexam.c
+++ src/backend/access/index/indexam.c
@@ -379,7 +379,7 @@ index_rescan(IndexScanDesc scan,
 	Assert(nkeys == scan->numberOfKeys);
 	Assert(norderbys == scan->numberOfOrderBys);
 
-	/* Release resources (like buffer pins) from table accesses */
+	/* reset read stream and release buffer pins from table accesses */
 	if (scan->xs_heapfetch)
 		table_index_fetch_reset(scan->xs_heapfetch);
 
@@ -467,7 +467,7 @@ index_restrpos(IndexScanDesc scan)
 	SCAN_CHECKS;
 	CHECK_SCAN_PROCEDURE(amgetbatch);
 
-	/* release resources (like buffer pins) from table accesses */
+	/* reset read stream and release buffer pins from table accesses */
 	if (scan->xs_heapfetch)
 		table_index_fetch_reset(scan->xs_heapfetch);
 

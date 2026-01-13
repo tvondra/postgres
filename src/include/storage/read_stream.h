1e49b695d0af74d9a34325aa31c9db42c8230766
Tomas Vondra <tomas@vondra.me>
2026-01-13 20:44:14 +0100

    Introduce read_stream_{pause,resume,yield}().

--- src/include/storage/read_stream.h
+++ src/include/storage/read_stream.h
@@ -99,6 +99,9 @@ extern ReadStream *read_stream_begin_smgr_relation(int flags,
 												   ReadStreamBlockNumberCB callback,
 												   void *callback_private_data,
 												   size_t per_buffer_data_size);
+extern BlockNumber read_stream_pause(ReadStream *stream);
+extern void read_stream_resume(ReadStream *stream);
+extern BlockNumber read_stream_yield(ReadStream *stream);
 extern void read_stream_reset(ReadStream *stream);
 extern void read_stream_end(ReadStream *stream);
 

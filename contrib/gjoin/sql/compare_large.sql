1b444314d46c80457006982d41238b4e0b7fff06
Tomas Vondra <tomas@vondra.me>
2026-05-15 14:15:06 +0200

    more tests

--- contrib/gjoin/sql/compare_large.sql
+++ contrib/gjoin/sql/compare_large.sql
@@ -49,5 +49,30 @@ SELECT compare_joins('SELECT * FROM t1 JOIN t2 ON (t1.a = t2.c)');
 SELECT compare_joins('SELECT * FROM t1 JOIN t2 ON (t1.a = t2.c AND t1.b < t2.d)');
 
 
+TRUNCATE t1;
+TRUNCATE t2;
+
+INSERT INTO t1 SELECT mod(i, 100000), 100000 * random() FROM generate_series(1,1000000) s(i);
+INSERT INTO t2 SELECT mod(i, 100000), 100000 * random() FROM generate_series(1,1000000) s(i);
+
+VACUUM ANALYZE t1;
+VACUUM ANALYZE t2;
+
+SELECT compare_joins('SELECT * FROM t1 JOIN t2 ON (t1.a = t2.c)');
+SELECT compare_joins('SELECT * FROM t1 JOIN t2 ON (t1.a = t2.c AND t1.b < t2.d)');
+
+
+TRUNCATE t1;
+TRUNCATE t2;
+
+INSERT INTO t1 SELECT mod(i, 100000), 100000 * random() FROM generate_series(1,1000000) s(i) ORDER BY random();
+INSERT INTO t2 SELECT mod(i, 100000), 100000 * random() FROM generate_series(1,1000000) s(i) ORDER BY random();
+
+VACUUM ANALYZE t1;
+VACUUM ANALYZE t2;
+
+SELECT compare_joins('SELECT * FROM t1 JOIN t2 ON (t1.a = t2.c)');
+SELECT compare_joins('SELECT * FROM t1 JOIN t2 ON (t1.a = t2.c AND t1.b < t2.d)');
+
 DROP TABLE t1;
 DROP TABLE t2;
